69b4bc2a9616948daade45e1fd0a648c09355246
Jianghao Wu <jw4455@columbia.edu>
2023-10-04 23:13:41 +0000

    part 3 complete 1.29

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,20 +9,18 @@ class MoviesController < ApplicationController
   def index
     
     @all_ratings = Movie.all_ratings
-
     if params[:order_by].nil?
       session[:order_by] = session[:order_by]
     elsif session[:order_by].nil?
       session[:order_by] = params[:order_by]
     end
-
     @ratings_to_show = []
     if params[:ratings].nil? && session[:ratings].nil?
     	@movies = Movie.all.order(params[:order_by])
       session[:ratings] = Movie.all_ratings
       @ratings_to_show = Hash[session[:ratings].collect{|i|[i, "1"]}]
-      redirect_to movies_path(:ratings => Hash[@ratings_to_show])
-    elsif !params[:ratings].nil?
+      redirect_to movies_path(:ratings => @ratings_to_show)
+    elsif !params[:ratings].nil? #!session[:ratings].nil?
       @ratings_to_show = params[:ratings]
       session[:ratings] = @ratings_to_show.keys
       @movies = Movie.with_ratings(session[:ratings]).order(params[:order_by])
@@ -30,6 +28,7 @@ class MoviesController < ApplicationController
       @movies = Movie.with_ratings(session[:ratings]).order(params[:order_by])
       @ratings_to_show = Hash[session[:ratings].collect{|i|[i, "1"]}]
       redirect_to movies_path(:ratings =>@ratings_to_show , :order_by =>session[:order_by])
+    end
 
     if params[:order_by] == 'release_date'
       @release_date_style = 'bg-warning hilite'
