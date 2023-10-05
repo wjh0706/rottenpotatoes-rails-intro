bf28642eea62cc92359c497b4a91c5ba89b0db6a
Jianghao Wu <jw4455@columbia.edu>
2023-10-05 01:16:48 +0000

    part 3 complete 1.4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,7 +32,7 @@ class MoviesController < ApplicationController
     else
       #@movies = Movie.with_ratings(session[:ratings]).order(params[:order_by])
       #@ratings_to_show = Hash[session[:ratings].collect{|i|[i, "1"]}]
-      redirect_to movies_path(ratings: @ratings_to_show, order_by: session[:order_by])
+      redirect_to movies_path(ratings: Hash[session[:ratings].collect{|i|[i, "1"]}], order_by: session[:order_by])
     end
     @ratings_to_show = Hash[session[:ratings].collect{|i|[i, "1"]}]
     @movies = Movie.with_ratings(@ratings_to_show.keys)
